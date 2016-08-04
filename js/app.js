69632125d272d9c4cd0583c51d93d46a73aa19bf
dingqian <302965061@qq.com>
2016-08-04 14:29:46 +0800

    study state and props

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-// TODO
+ const App= React.createClass({
+     getInitialState: function () {
+         return {
+             count: 0
+         }
+     },
+     add:function () {
+         this.setState({count: this.state.count + 1});
+     },
+     render:function() {
+     return <div>
+         <Count count={this.state.count} addC={this.add}/>
+         <NotesList />
+     </div>
+     }
+ });
+
+  const Count = React.createClass({
+      adds:function(){
+        this.props.addC();
+      },
+      render:function() {
+      return <div>
+          {this.props.count}
+          <button onClick={this.adds}>+</button>
+      </div>
+      }
+  });
+ 
+ ReactDOM.render(<App />, document.getElementById('content'));
